10b4498982bc817e7160a820075d9ee951b3e747
yypai <45518828+yypai@users.noreply.github.com>
2019-07-09 13:44:56 -0400

    Create helvetica.py

---
+++ helvetica.py
@@ -0,0 +1,7 @@
+# not always this file name:
+# in shell: 
+# rm ~/.cache/.matplotlib/fontlist-v310.json
+
+import matplotlib 
+matplotlib.rcParams['font.family'] = 'sans-serif'
+matplotlib.rcParams['font.sans-serif'] = ['Helvetica']
