6e9741a3f6938a0ea4043509bb618de17a8db6b8
yypai <45518828+yypai@users.noreply.github.com>
2021-05-13 23:05:53 -0400

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -1,22 +1,22 @@
 import plotly.graph_objects as go
-
-def plot_plotly(files, x, y, sep = ','):
+def plot_plotly(files, x, y, sep = ',', 
+                plot_title = 'title', legend_tile = 'legend', 
+                filename = 'export.html'):
     fig = go.Figure()
     for file in files:
         df = pd.read_csv(file, sep = sep)
-        fig.add_trace(go.Scatter(x=df[x], y=df[y], name = file))
+        fig.add_trace(go.Scatter(x=df[x], y=df[y].rolling(1).mean(), name = file))
         
     fig.update_layout(template='plotly_white')
-    
     fig.update_layout(
-    title="Plot Title",
-    xaxis_title=x,
-    yaxis_title=y,
-    legend_title="Legend Title",
-    font=dict(
-        family="Sans-Serif",
-        size=18,
-        color="Black"
-    ))
- 
+        title=plot_title,
+        xaxis_title=x,
+        yaxis_title=y,
+        legend_title=legend_tile,
+        font=dict(
+            family="Sans-Serif",
+            size=18,
+            color="Black"
+        ))
+    fig.write_html(filename)
     fig.show()   
